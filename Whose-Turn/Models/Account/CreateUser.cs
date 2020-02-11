4912fd6ec13c615f3520c8845e2958e8e91fbeed
Kevo Macartney <kevomacartney@hotmail.com>
2020-02-11 16:53:47 +0000

    MVC models

--- Whose-Turn/Models/Account/CreateUser.cs
+++ Whose-Turn/Models/Account/CreateUser.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Whose_Turn.Models.Account
 {
-    public class CreateUser
+    public class CreateUserModel
     {
-        public CreateUser()
-        {
-        }
+        /// <summary>
+        /// Gets or sets the first name
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address
+        /// </summary>
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the users password
+        /// </summary>
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$", MatchTimeoutInMilliseconds = 100,
+            ErrorMessage = "Password must containt Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:")]
+        public string Password { get; set; }
     }
 }
--- Whose-Turn/Models/Todo/CreateTodoModel.cs
+++ Whose-Turn/Models/Todo/CreateTodoModel.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Whose_Turn.Models.Todo
 {
     public class CreateTodoModel
     {
-        public CreateTodoModel()
-        {
-        }
+        /// <summary>
+        /// Gets or sets the due date
+        /// </summary>
+        public DateTime DueOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the to-do task
+        /// </summary>
+        public string Task { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of users the task is assinged to
+        /// </summary>
+        public List<Guid> AssignedTo { get; set; }
     }
 }
